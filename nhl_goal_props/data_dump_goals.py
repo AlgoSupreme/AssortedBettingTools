eb4cea29da9b594ca7b177e7411951ce7586cf2a
AlgoSupreme <pcy2n2hy2m@privaterelay.appleid.com>
2026-01-15 16:02:13 -0500

    Add files via upload
    FIXED CRITICAL ERROR. Year was spawning in the future for start date

--- nhl_goal_props/data_dump_goals.py
+++ nhl_goal_props/data_dump_goals.py
@@ -18,10 +18,10 @@ class DataDumper():
         self.SaveDir = "data_dump_goals"
         if datetime.now().date().month < 6:
             self.season = f"{int(datetime.now().date().year)-1}{int(datetime.now().date().year)}"
-            self.StartDate = str(datetime.now().date().year) + "-10-01"
+            self.StartDate = str(datetime.now().date().year-1) + "-10-01"
         else:
             self.season = f"{int(datetime.now().date().year)}{int(datetime.now().date().year)+1}"
-            self.StartDate = str(datetime.now().date().year) + "-10-01"
+            self.StartDate = str(datetime.now().date().year-1) + "-10-01"
         self.EndDate = (datetime.now().date() - timedelta(days=1))
         self.Roster = {}
 
